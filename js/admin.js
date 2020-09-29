8d97d97971547c5054c857ec12b4a34c531339a9
Ben Johnston <benj@princeton.edu>
2020-09-29 07:28:38 -0400

    working

--- js/admin.js
+++ js/admin.js
@@ -1,13 +1,43 @@
 jQuery( document ).ready(function() {
 
 
+
+
+function createLayersFromJson(data) {
+  const layers = [];
+  
+  jQuery.each(data, function(geo,id){
+    L.geoJSON(geo, {
+      pointToLayer: (feature, latlng) => {
+        if (feature.properties.radius) {
+          return new L.Circle(latlng, feature.properties.radius);
+        } else {
+          return new L.Marker(latlng);
+        }
+      },
+      onEachFeature: (feature, layer) => {
+        layer.push(layer);
+      },
+    });
+  });
+  
+  return layers;
+};
+
+
+
+
+
+
+
+
+
+
     
   // tile layers
   //http://www.google.cn/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}
   //https://{s}.tile.osm.org/{z}/{x}/{y}.png
-  
-
-	console.log(vars.mapdata);
+ 
   
   
 	var home_coords = [39.952718, -75.164093 ];
@@ -25,8 +55,12 @@ jQuery( document ).ready(function() {
 	  var mylayer = new L.FeatureGroup();
 	}
 	else {
+	  //var geojson = JSON.parse(vars.mapdata.geojson);
+	  //console.log(geojson);
+	  //var featureLayer = createLayersFromJson(geojson);
 	  var featureLayer = JSON.parse(vars.mapdata.geojson);
 	  var mylayer = L.geoJSON(featureLayer).addTo(map);
+	  //featureLayer.addTo(map);
 	  map.fitBounds(mylayer.getBounds());
 	}
 
@@ -42,22 +76,27 @@ jQuery( document ).ready(function() {
      map.addControl(drawControl);
      
      
+	function addRadius(geojson,radius){
+	 var index = geojson.features.length - 1;
+	 geojson.features[index].properties.radius = radius;
+	 return geojson;
+	}      
      
-      
-     
-
-  map.on('draw:created', function (e) {
-  
-
-    var type = e.layerType,
-        layer = e.layer;
-
-    mylayer.addLayer(layer);
-    var geojson = mylayer.toGeoJSON();
-    jQuery('#GeoJSON').val(JSON.stringify(geojson));
 
 
-  });
+	map.on('draw:created', function (e) {
+	  var type = e.layerType,
+	  layer = e.layer;
+
+	  mylayer.addLayer(layer);
+	  var geojson = mylayer.toGeoJSON();
+	  /*
+ 	  if(type == 'circle' || type == 'circlemarker') {
+	    geojson = addRadius(geojson,layer._mRadius);
+	  }
+	  */
+	  jQuery('#GeoJSON').val(JSON.stringify(geojson));
+	});
 
 
   map.on('draw:edited', function (e) {
